bd0ad9d2a3d82e73f45d7ac60e0b3bfa431ee4ee
Elizabeth Mitchell <elizabeth@sassafras.coop>
2016-01-27 11:35:57 -0500

    3965: Replace php template with updated got drought template

--- tool.php
+++ tool.php
@@ -3,21 +3,21 @@
 <link rel="stylesheet" type="text/css"
   href="<?php echo $url ?>/water-footprint-wp/css/water-footprint.css" />
 <link rel="stylesheet" type="text/css"
+  href="<?php echo $url ?>/water-footprint-wp/css/water-footprint-theme.css" />
+<link rel="stylesheet" type="text/css"
   href="<?php echo $url ?>/water-footprint-wp/css/got-drought.css" />
 <script src="<?php echo $url ?>/water-footprint-wp/vendor/jquery/jquery-1.11.3.min.js"></script>
 <link rel="stylesheet" type="text/css" href="<?php echo $url ?>/water-footprint-wp/vendor/jquery-ui/jquery-ui.min.css" />
 <link rel="stylesheet" type="text/css" href="<?php echo $url ?>/water-footprint-wp/vendor/jquery-ui/jquery-ui.structure.min.css" />
 <link rel="stylesheet" type="text/css" href="<?php echo $url ?>/water-footprint-wp/vendor/jquery-ui/jquery-ui.theme.min.css" />
 <script src="<?php echo $url ?>/water-footprint-wp/vendor/jquery-ui-autocomplete/jquery-ui.min.js"></script>
-<script src="<?php echo $url ?>/water-footprint-wp/js/foods.js"></script>
+<script src="<?php echo $url ?>/water-footprint-wp/js/got-drought/foods.js"></script>
 <script src="<?php echo $url ?>/water-footprint-wp/js/units.js"></script>
 
 <section class="water-footprint">
   <header>
     <h1>How thirsty is your food?</h1>
     <p class="instructions">Select an item below to display the amount of water used to produce the food.</p>
-
-    <p class="credits">All water footprint statistics are from The Water Footprint Network's <a href="http://waterfootprint.org/en/resources/interactive-tools/product-gallery/" target="_blank">interactive tool</a>.</p>
   </header>
 
   <input class="choose-item" value="" />
@@ -33,7 +33,6 @@
     <div class="no-info">
     </div>
   </section>
-  </section>
 </section>
 
-<script src="<?php echo $url ?>/water-footprint-wp/js/calculator.js"></script>
+<script src="<?php echo $url ?>/water-footprint-wp/js/got-drought/calculator.js"></script>
