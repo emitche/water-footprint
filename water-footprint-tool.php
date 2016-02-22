0083013e9fa641e98d9dcdb8ea665cec391ef0bb
Elizabeth Mitchell <elizabeth@sassafras.coop>
2016-02-22 10:01:12 -0500

    Upgrade tool version

--- water-footprint-tool.php
+++ water-footprint-tool.php
@@ -1,30 +1,30 @@
 <?php
 /**
  * @package Water_Footprint
- * @version 0.2
+ * @version 1.0
  */
 /*
 Plugin Name: Water Footprint
-Plugin URI: http://github.com/emitche/water-footprint-wp/
-Description: A simple plugin to display the water footprint of specific foods.
+Plugin URI: http://github.com/emitche/water-footprint
+Description: A simple tool and WordPress plugin to display the water footprint of specific foods.
 Author: Elizabeth Mitchell
-Version: 0.2
+Version: 1.0
 Author URI: http://github.com/emitche
 */
 
 # defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
-function water_footprint_shortcode() {
-    return load_water_footprint();
+function water_footprint_tool_shortcode() {
+    return load_water_footprint_tool();
 }
 
-function water_footprint_register_shortcode() {
-    add_shortcode( 'water-footprint', 'water_footprint_shortcode' );
+function water_footprint_tool_register_shortcode() {
+    add_shortcode( 'water-footprint-tool', 'water_footprint_tool_shortcode' );
 }
 
-add_action( 'init', 'water_footprint_register_shortcode' );
+add_action( 'init', 'water_footprint_tool_register_shortcode' );
 
-function load_water_footprint(){
+function load_water_footprint_tool(){
   ob_start();
   include('wordpress/tool.php');
   return ob_get_clean();
--- wordpress/tool.php
+++ wordpress/tool.php
@@ -1,26 +1,52 @@
 <?php $url = plugins_url(); ?>
 
-<link rel="stylesheet" type="text/css"
-  href="<?php echo $url ?>/water-footprint-wp/css/water-footprint.css" />
-<link rel="stylesheet" type="text/css"
-  href="<?php echo $url ?>/water-footprint-wp/css/water-footprint-theme.css" />
-<link rel="stylesheet" type="text/css"
-  href="<?php echo $url ?>/water-footprint-wp/css/got-drought.css" />
-<script src="<?php echo $url ?>/water-footprint-wp/vendor/jquery/jquery-1.11.3.min.js"></script>
-<link rel="stylesheet" type="text/css" href="<?php echo $url ?>/water-footprint-wp/vendor/jquery-ui/jquery-ui.min.css" />
-<link rel="stylesheet" type="text/css" href="<?php echo $url ?>/water-footprint-wp/vendor/jquery-ui/jquery-ui.structure.min.css" />
-<link rel="stylesheet" type="text/css" href="<?php echo $url ?>/water-footprint-wp/vendor/jquery-ui/jquery-ui.theme.min.css" />
-<script src="<?php echo $url ?>/water-footprint-wp/vendor/jquery-ui-autocomplete/jquery-ui.min.js"></script>
-<script src="<?php echo $url ?>/water-footprint-wp/js/got-drought/foods.js"></script>
-<script src="<?php echo $url ?>/water-footprint-wp/js/units.js"></script>
+<link rel="stylesheet" type="text/css" href="<?php echo $url ?>/water-footprint/css/water-footprint.css" />
+<link rel="stylesheet" type="text/css" href="<?php echo $url ?>/water-footprint/css/water-footprint-theme.css" />
+<link rel="stylesheet" type="text/css" href="<?php echo $url ?>/water-footprint/css/got-drought.css" />
+
+<!-- jQuery -->
+<script src="<?php echo $url ?>/water-footprint/vendor/jquery/jquery-1.11.3.min.js"></script>
+
+<link rel="stylesheet" type="text/css" href="<?php echo $url ?>/water-footprint/vendor/jquery-ui/jquery-ui.min.css" />
+<link rel="stylesheet" type="text/css" href="<?php echo $url ?>/water-footprint/vendor/jquery-ui/jquery-ui.structure.min.css" />
+<link rel="stylesheet" type="text/css" href="<?php echo $url ?>/water-footprint/vendor/jquery-ui/jquery-ui.theme.min.css" />
+
+<script src="<?php echo $url ?>/water-footprint/vendor/jquery-ui-autocomplete/jquery-ui.min.js"></script>
+
+<!-- Got Drough Data Set and Calculations -->
+<script src="<?php echo $url ?>/water-footprint/js/got-drought/gd_foods.js"></script>
+<script src="<?php echo $url ?>/water-footprint/js/units.js"></script>
+<script src="<?php echo $url ?>/water-footprint/js/got-drought/gd_calculator.js"></script>
 
 <section class="water-footprint">
   <header>
     <h1>How thirsty is your food?</h1>
-    <p class="instructions">Select an item below to display the amount of water used to produce the food.</p>
+    <p class="instructions">Type the name of a food to display the amount of water used to produce the food.</p>
   </header>
 
-  <input class="choose-item" value="" />
+  <input name="choose-food" class="choose-item" value="" aria-label="Type Name of Food" />
+
+  <div class="additonal-instructions">
+    <h3>Instructions</h2>
+    <ol>
+      <li>
+        Begin by typing the name of a food. As you type, a list of foods matching will appear.
+      </li>
+      <li>Select the food.
+        <ul>
+          <li>
+            Click or touch the food for which you would like water data displayed.
+          </li>
+          <li>
+            On a keyboard, use the up and down arrows to access the matching foods. Hit "Enter" when are ready to select an item.
+          </li>
+        </ul>
+      </li>
+      <li>
+        Once you have clicked or touched the food or hit "Enter", the food you selected will be displayed below.
+      </li>
+    </ol>
+  </div>
 
   <section class="result">
     <h1 class="heading"></h1>
@@ -35,4 +61,4 @@
   </section>
 </section>
 
-<script src="<?php echo $url ?>/water-footprint-wp/js/got-drought/calculator.js"></script>
+<script src="<?php echo $url ?>/water-footprint/js/got-drought/gd_calculator.js"></script>
